b519879348e1ec0e5f68506317ac0cf7c62ecdea
Mark Dawson <mark.dawson@swansea.ac.uk>
2020-12-03 00:11:19 +0000

    day 2 - Part 2

--- day_02.clj
+++ day_02.clj
@@ -16,18 +16,48 @@
   (count
    (filter (partial = character) string)))
 
-(defn validate-passwd
-  "Return true if `line` specifies a valid rule/password combination."
+(defn parse-line
+  "Parse regular expression into map of components"
   [line]
-  (let [[all min max character passwd]
-        (re-find #"(\d+)-(\d+) (\S+): (\S+)" line)
-        char-count (count-chars (first character) passwd)]
-    (and (>= char-count (read-string min))
-         (<= char-count (read-string max)))))
+  (as-> line v
+    (re-find #"(\d+)-(\d+) (\S+): (\S+)" v)
+    (zipmap [:all :num1 :num2 :char :passwd] v)
+    (update-in v [:num1] read-string)
+    (update-in v [:num2] read-string)
+    (update-in v [:char] first)))
 
-;; Use function to  valid passwords
-(->>
- password-input                       ;; -> list of lines
- (map validate-passwd)                ;; -> list of valid/invalid flags
- (filter #'identity)                  ;; -> filter out invalid flags
- count)                               ;; -> count entries
+(defn policy-1
+  "Return true if password data `p` specifies a valid rule/password combination by policy 1."
+  [{:keys [num1 num2 passwd char]}]
+  (let [char-count (count-chars
+                    char
+                    passwd)]
+    (and (>= char-count num1)
+         (<= char-count num2))))
+
+;; Count passwords
+(defn count-valid
+  "Return number of lines in `password-input` satisfying `policy`"
+  [password-input policy]
+  (->>
+   password-input       
+   (map parse-line)     ;; extract line data
+   (map policy)         ;; apply policy
+   (filter #'identity)  ;; filter out nil values
+   count))              ;; count entries
+
+;; Solution to part 1
+(count-valid password-input policy-1)
+
+;; Part 2
+(defn policy-2 [{:keys [num1 num2 passwd char]}]
+  "Return true if passwd specifies a valid rule/password combination by policy 2."
+  (= 1 (->>
+        [num1 num2]
+        (map dec)
+        (map #(= (nth passwd %1) char))
+        (filter #'identity)
+        count))))
+
+;; Solution to part 2
+(count-valid password-input policy-2)
